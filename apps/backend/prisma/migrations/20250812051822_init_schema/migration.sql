6118602c4f087742ed3d9b6621ac6fc55a0e5fa6
Adriana Bazan <adriana.bazan47@gmail.com>
2025-09-02 20:53:46 -0600

    db: add User.updatedAt

--- apps/backend/prisma/migrations/20250812051822_init_schema/migration.sql
+++ apps/backend/prisma/migrations/20250812051822_init_schema/migration.sql
@@ -64,3 +64,6 @@ ALTER TABLE "public"."InventoryMember" ADD CONSTRAINT "InventoryMember_userId_fk
 
 -- AddForeignKey
 ALTER TABLE "public"."Item" ADD CONSTRAINT "Item_inventoryId_fkey" FOREIGN KEY ("inventoryId") REFERENCES "public"."Inventory"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "User"
+ADD COLUMN "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
